9cba992acf22b5ba7a890a4c247fcae021d7d304
marine147 <90772433+marine147@users.noreply.github.com>
2021-09-15 20:46:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ const CONFIG = {
     introTitle: 'hee lu cậu!',
     introDesc: `Tớ có điều này muốn hỏi cậu,nhớ trả lời thật lòng nha`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Cậu thích tớ có phải không 😙',
+    desc: 'Cậu ko trả lời hoặc thoát ra tức cậu là vợ tớ đónha:> ',
+    btnYes: 'ừm mình thích cậu <33',
+    btnNo: 'Không! tôi ghét cậu :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại thích tớ <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Mình biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'còn giờ chờ gì nữa mà ko inbox tớ đi nào.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100027430421519'
 }
